f338546aee32138bbd07bbf795a67218c4725e02
Szilvi <szilvia.bogadi@student.upt.ro>
2024-02-23 17:44:46 +0200

    Added camera dependencies

--- app/src/main/java/com/szi/plantbuddy/OptionsActivity.java
+++ app/src/main/java/com/szi/plantbuddy/OptionsActivity.java
@@ -1,7 +1,9 @@
 package com.szi.plantbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -27,11 +29,44 @@ public class OptionsActivity extends AppCompatActivity implements AdapterView.On
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:
+                // Night mode is not active, we're using the light theme
+                Log.d("debug", "Night mode is not active, we're using light theme");
+                spinner.setSelection(0);
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                // Night mode is active, we're using dark theme
+                Log.d("debug", "Night mode is active, we're using dark theme");
+                spinner.setSelection(1);
+                break;
+            case Configuration.UI_MODE_NIGHT_UNDEFINED:
+                // Night mode state is undefined, could be following the system
+                Log.d("debug", "Night mode state is undefined, could be following the system");
+                spinner.setSelection(2);
+                break;
+        }
+
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Log.d("debug", parent.getItemAtPosition(position).toString());
+        String themeOption = parent.getItemAtPosition(position).toString();
+        Log.d("debug", themeOption);
+
+        switch (themeOption){
+            case "Light":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+            case "Dark":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+            case "System Default":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                break;
+        }
     }
 
     @Override
