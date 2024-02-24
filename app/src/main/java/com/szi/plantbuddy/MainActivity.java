ce36bd919dd458c8f4343ac4ff271fe4c61ebf5d
OnigiriRolls <szilvia.bogadi@student.upt.ro>
2024-02-24 21:17:07 +0200

    Added better photo functionality

--- app/src/main/java/com/szi/plantbuddy/MainActivity.java
+++ app/src/main/java/com/szi/plantbuddy/MainActivity.java
@@ -1,12 +1,12 @@
 package com.szi.plantbuddy;
 
 import android.app.Activity;
-import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.os.Build;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -16,18 +16,13 @@ import android.widget.Toast;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureException;
-import androidx.camera.core.Preview;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
-import com.google.common.util.concurrent.ListenableFuture;
-
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
-import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends BaseActivity {
     private static final int MY_CAMERA_REQUEST_CODE = 100;
@@ -35,7 +30,7 @@ public class MainActivity extends BaseActivity {
 
     ActivityResultLauncher<Intent> cameraActivityResultLauncher;
     private ImageView imageView;
-    private ImageCapture imageCapture;
+    private String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,71 +42,22 @@ public class MainActivity extends BaseActivity {
                 new ActivityResultContracts.StartActivityForResult(),
                 result -> {
                     if (result.getResultCode() == Activity.RESULT_OK) {
-                        Intent data = result.getData();
-                        if (data != null && data.getExtras() != null) {
-                            Bitmap photo = (Bitmap) data.getExtras().get("data");
-                            if (photo != null)
-                                imageView.setImageBitmap(photo);
+                        try {
+                            Bitmap imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(currentPhotoPath));
+                            imageView.setImageBitmap(imageBitmap);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
                         }
-
                     }
                 });
-
-        startCamera();
     }
 
     public void onClick(View view) {
         if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{android.Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
         } else {
-            takePhoto();
+            startCameraSimple();
         }
-    }
-
-    private void takePhoto() {
-        // Get a stable reference of the modifiable image capture use case
-        ImageCapture imageCapture = this.imageCapture;
-        if (imageCapture == null) {
-            return;
-        }
-
-        // Create time stamped name and MediaStore entry.
-        String name = new SimpleDateFormat(FILENAME_FORMAT, Locale.getDefault())
-                .format(System.currentTimeMillis());
-
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
-        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
-
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image");
-        }
-
-        // Create output options object which contains file + metadata
-        ImageCapture.OutputFileOptions outputOptions = new ImageCapture.OutputFileOptions.Builder(
-                getContentResolver(),
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                contentValues
-        ).build();
-
-        // Set up image capture listener, which is triggered after the photo has been taken
-        imageCapture.takePicture(
-                outputOptions,
-                ContextCompat.getMainExecutor(this),
-                new ImageCapture.OnImageSavedCallback() {
-                    @Override
-                    public void onError(@NonNull ImageCaptureException exc) {
-                        Log.e("debug", "Photo capture failed: " + exc.getMessage(), exc);
-                    }
-
-                    @Override
-                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults output) {
-                        String msg = "Photo capture succeeded: " + output.getSavedUri();
-                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-                        Log.d("debug", msg);
-                    }
-                }
-        );
     }
 
     @Override
@@ -119,40 +65,43 @@ public class MainActivity extends BaseActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == MY_CAMERA_REQUEST_CODE) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                takePhoto();
+                startCameraSimple();
             } else {
                 Toast.makeText(this, "Camera permission is denied", Toast.LENGTH_LONG).show();
             }
         }
     }
 
-    private void startCamera() {
-        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-
-        cameraProviderFuture.addListener(() -> {
-            try {
-                // Used to bind the lifecycle of cameras to the lifecycle owner
-                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-
-                imageCapture = new ImageCapture.Builder().build();
-
-                // Select back camera as a default
-                CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
-
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll();
-
-                // Bind use cases to camera
-                cameraProvider.bindToLifecycle(this, cameraSelector, imageCapture);
-
-            } catch (ExecutionException | InterruptedException exc) {
-                Log.e("debug", "Error getting camera provider", exc);
-            }
-        }, ContextCompat.getMainExecutor(this));
-    }
-
     private void startCameraSimple() {
         Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraActivityResultLauncher.launch(cameraIntent);
+
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            Toast.makeText(this, "There was an error!", Toast.LENGTH_LONG).show();
+            Log.i("debug", "IOException");
+        }
+
+        if (photoFile != null) {
+            Uri photoURI = FileProvider.getUriForFile(getApplicationContext(), getApplicationContext().getPackageName() + ".provider", photoFile);
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            cameraActivityResultLauncher.launch(cameraIntent);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat(FILENAME_FORMAT, Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        currentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
     }
 }
