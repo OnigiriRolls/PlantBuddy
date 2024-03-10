acee360100dfe0805b66fdd9b995ccb5f87ef771
Szilvi <szilvia.bogadi@student.upt.ro>
2024-03-10 19:47:51 +0200

    Worked on plant item layout

--- app/src/main/java/com/szi/plantbuddy/ui/PlantViewHolder.java
+++ app/src/main/java/com/szi/plantbuddy/ui/PlantViewHolder.java
@@ -3,6 +3,7 @@ package com.szi.plantbuddy.ui;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,9 +25,11 @@ public class PlantViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void bind(Plant plant) {
-        TextView plantNameTextView = itemView.findViewById(R.id.plantNameTextView);
+        TextView plantNameTextView = itemView.findViewById(R.id.tPlantName);
+        ImageView imageView = itemView.findViewById(R.id.plantImage);
         if (plant != null) {
             plantNameTextView.setText(plant.getCommonName());
+            imageView.setImageBitmap(plant.getImage().getThumbnail());
         }
     }
 }
